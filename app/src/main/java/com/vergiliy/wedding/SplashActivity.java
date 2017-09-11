9a4ca09c8c13dd8358569eed1119fe2e0df911af
Vergiliy <vergiliy-@mail.ru>
2017-09-11 21:38:07 +0300

    Complete splash screen

--- app/src/main/java/com/vergiliy/wedding/SplashActivity.java
+++ app/src/main/java/com/vergiliy/wedding/SplashActivity.java
@@ -4,26 +4,102 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 // Splash screen; open before load MainActivity
 public class SplashActivity extends AppCompatActivity {
 
-    final int SPLASH_DISPLAY = 2000; // Duration splash screen
-    final Context context = this;
+    public static final int DURATION_SPLASH = 5000;
+    public static final int DURATION_LOGO = 1000;
+    public static final int DURATION_BUTTON = 1000;
+    public static final int DURATION_OTHER = 500;
+    public static final int DELAY_START = 200;
+    public static final int DELAY_ITEM = 300;
+    public static final int DELAY_BETWEEN = 500;
+
+    public final Context context = this;
+
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
+        // Go to fullscreen without navigation bar
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        animate(); // Start animation
 
-        // Show splash scree duration several seconds then start MainActivity
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-                finish();
+        // Show splash screen duration several seconds then start MainActivity
+        handler.postDelayed(runnable, DURATION_SPLASH);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Interrupt thread
+        handler.removeCallbacks(runnable);
+    }
+
+    // Jump to MainActivity (call when the button clicked)
+    public void start(View view){
+        Intent intent = new Intent(context, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // Animation elements
+    private void animate() {
+        ImageView logoImageView = (ImageView) findViewById(R.id.splash_logo);
+        ViewGroup container = (ViewGroup) findViewById(R.id.splash_container);
+
+        // Animation logo
+        ViewCompat.animate(logoImageView)
+                .translationY(-250)
+                .scaleX(3)
+                .scaleY(3)
+                .setStartDelay(DELAY_START)
+                .setDuration(DURATION_LOGO)
+                .setInterpolator(new DecelerateInterpolator(1.2f))
+                .start();
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View view = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            // Animation buttons
+            if (!(view instanceof Button)) {
+                viewAnimator = ViewCompat.animate(view)
+                        .translationY(50)
+                        .alpha(1)
+                        .setStartDelay((DELAY_ITEM * i) + DELAY_BETWEEN)
+                        .setDuration(DURATION_BUTTON);
+            // Animation another elements
+            } else {
+                viewAnimator = ViewCompat.animate(view)
+                        .scaleY(1)
+                        .scaleX(1)
+                        .setStartDelay((DELAY_ITEM * i) + DELAY_BETWEEN)
+                        .setDuration(DURATION_OTHER);
             }
-        }, SPLASH_DISPLAY);
+
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }
     }
 }
